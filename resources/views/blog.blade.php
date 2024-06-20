f0be30d2ae81b3735ffb073d4d6d53a55d2a6c42
adhamajid <adha1900018252@webmail.uad.ac.id>
2024-06-20 08:46:46 +0700

    firstcommit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Welcome to My Blog</h3>
+    <p>Orang paling kece, keren, seru.. yuhuu...</p>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <p>Halo Kembali lagi Dengan Saya Adha Syah Majid</p>
+    <p>ORang paling kece, keren, seru.. yuhuu...</p>
+</x-layout>
